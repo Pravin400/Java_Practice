5f7b0dfacda8f4f4f454685005d216ffe9010324
Pravin400 <pravintambe5671@gmail.com>
2026-01-04 14:37:40 +0530

    Type casting with the come trick i use sleep thread here

---
+++ src/Interview_Practice_Question/recap_basics/IdentifierCaseSensitivity.java
@@ -0,0 +1,9 @@
+package Interview_Practice_Question.recap_basics;
+
+public class IdentifierCaseSensitivity {
+    public static void main(String[] args) {
+        int a = 10;
+        int A = 10;
+        System.out.println("The sum of a + A = "+(a+A));
+    }
+}

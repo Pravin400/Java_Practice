07347f181caa69349759ff3e1fd8770e8c25d251
Pravin400 <pravintambe5671@gmail.com>
2026-01-04 18:49:43 +0530

    Array problem solve having a take size of array from use and build also taking elements of array from user and perform operations on array sum, average, Read

--- src/Interview_Practice_Question/array_recap/ArraySumAndAverage.java
+++ src/Interview_Practice_Question/array_recap/ArraySumAndAverage.java
@@ -1,4 +1,48 @@
 package Interview_Practice_Question.array_recap;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArraySumAndAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int arraySize =  sc.nextInt();
+        int[] arr = new int[arraySize];
+        System.out.println("Enter the elements of the array : ");
+        for(int i=0;i<arraySize;i++){
+
+            System.out.print("Enter the element "+(1+i)+" in the array : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("array Elements are : "+Arrays.toString(arr));
+
+//        int arraySum = Arrays.stream(arr).sum();
+//        int arrayAverage = Arrays.stream(arr).sum();
+        System.out.print("array Sum is : "+arraySum(arr));
+        System.out.print("\narray Average is : "+arrayAverage(arr));
+        System.out.print("\nelements of Array is : ");
+        printArray(arr);
+    }
+    public static void printArray(int[] arr){
+        for(int i=0;i<arr.length;i++){
+
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static int arraySum(int[] arr){
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            if (arr.length == 0) return 0;
+
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static int arrayAverage(int[] arr){
+        if (arr.length == 0) return 0;
+
+        int average = arraySum(arr)/arr.length;
+        return average;
+    }
 }

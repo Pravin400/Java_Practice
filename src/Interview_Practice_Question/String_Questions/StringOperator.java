60ad2165ebe225c55093ab45aa9d1d12abbc297a
Pravin400 <pravintambe5671@gmail.com>
2026-01-04 14:10:42 +0530

    String Practice

---
+++ src/Interview_Practice_Question/String_Questions/StringOperator.java
@@ -0,0 +1,11 @@
+package Interview_Practice_Question.String_Questions;
+
+public class StringOperator {
+    public static void main(String[] args) {
+        System.out.println('a'+0);
+        System.out.println('A'+0);
+        System.out.println("A"+'a'+'a'+10+'m');
+        System.out.println('A'+'a'+'a');
+        System.out.println(10+'a'+'a');
+    }
+}

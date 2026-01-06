bfd5a94a1d0934793178e692c4e6361226897541
Pravin400 <pravintambe5671@gmail.com>
2026-01-06 23:34:28 +0530

    In sentence how many are words present are counted and count displayed

---
+++ src/Interview_Practice_Question/String_Questions/WordCounter.java
@@ -0,0 +1,19 @@
+package Interview_Practice_Question.String_Questions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        System.out.println("Word Counter");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter Sentence : ");
+        String str = scan.nextLine();
+
+//        String [] words = str.split(" "); this can only skips only one space if sentence consists of more than one space still skips one space
+        String [] words = str.split("[\\s+]");
+        System.out.println("Original sentence is : '"+str+"'");
+        System.out.println("Sentence World Count is : "+words.length);
+    }
+}

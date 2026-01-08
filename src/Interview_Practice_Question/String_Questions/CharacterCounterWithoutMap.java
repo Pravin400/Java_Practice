30b6b834c772e1e5cdfd58ba2918ed80559ebf2c
Pravin400 <pravintambe5671@gmail.com>
2026-01-08 23:48:09 +0530

    Here Finds the count of alphabets, digits & special_characters with the help of ascii range and some loop logic

---
+++ src/Interview_Practice_Question/String_Questions/CharacterCounterWithoutMap.java
@@ -0,0 +1,32 @@
+package Interview_Practice_Question.String_Questions;
+
+import java.util.Scanner;
+
+public class CharacterCounterWithoutMap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String str = input.nextLine();
+
+        int alphabets = 0;
+        int digits = 0;
+        int specialChars = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ') {
+                continue;
+            }
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                alphabets++;
+            } else if ((ch >= '0' && ch <= '9')) {
+                digits++;
+            } else {
+                specialChars++;
+            }
+        }
+        System.out.println("Alphabets : " + alphabets);
+        System.out.println("Digits : " + digits);
+        System.out.println("SpecialChars : " + specialChars);
+    }
+}

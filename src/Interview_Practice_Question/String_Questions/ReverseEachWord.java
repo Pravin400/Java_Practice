a9c7461bbc321cfc8aec7f0ece6c28d9d2253020
Pravin400 <pravintambe5671@gmail.com>
2026-01-10 23:56:43 +0530

    Reversal of world in a String(sentence)

---
+++ src/Interview_Practice_Question/String_Questions/ReverseEachWord.java
@@ -0,0 +1,33 @@
+package Interview_Practice_Question.String_Questions;
+
+import java.util.Scanner;
+
+public class ReverseEachWord {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String str = input.nextLine();
+
+        String[] words = str.split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+
+            char[] charArray = words[i].toCharArray();
+            int start = 0;
+            int end = charArray.length - 1;
+
+            while (start < end) {
+                char temp = charArray[start];
+                charArray[start] = charArray[end];
+                charArray[end] = temp;
+                start++;
+                end--;
+            }
+
+            words[i] = new String(charArray);
+        }
+
+        System.out.println(String.join(" ", words));
+    }
+}
